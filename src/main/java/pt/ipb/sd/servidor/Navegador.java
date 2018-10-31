9d82537e374119ccc45797b9df294cc0e42675c3
hensou <rickftime@gmail.com>
2018-10-31 15:22:03 +0000

    Implementada todas as funções do navegador remoto

--- src/main/java/pt/ipb/sd/servidor/Navegador.java
+++ src/main/java/pt/ipb/sd/servidor/Navegador.java
@@ -1,12 +1,17 @@
 package pt.ipb.sd.servidor;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+
 import pt.ipb.sd.interfaces.NavegadorRemote;
 
 public class Navegador extends UnicastRemoteObject implements NavegadorRemote {
@@ -43,22 +48,45 @@ public class Navegador extends UnicastRemoteObject implements NavegadorRemote {
 
     @Override
     public boolean mkdir(String nome) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Path path = Paths.get(nome);
+        try {
+            Files.createDirectory(path);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     @Override
     public boolean touch(String nome) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Path path = Paths.get(nome);
+        if(Files.exists(path)) return false;
+
+        File file = new File(String.valueOf(path));
+        try {
+            return file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Falha ao criar o ficheiro");
+        }
+        return false;
     }
 
     @Override
     public boolean rm(String nome) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Path path = Paths.get(nome);
+        if(Files.notExists(path) || !Files.isRegularFile(path)) return false;
+        File file = new File(String.valueOf(path));
+        return file.delete();
     }
 
     @Override
     public boolean rmdir(String nome) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Path path = Paths.get(nome);
+        if(Files.notExists(path) || !Files.isDirectory(path)) return false;
+        File file = new File(String.valueOf(path));
+        return file.delete();
     }
 
     @Override
@@ -74,30 +102,41 @@ public class Navegador extends UnicastRemoteObject implements NavegadorRemote {
 
     @Override
     public String cat(String nome) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Path path = Paths.get(nome);
+        StringBuilder output = new StringBuilder();
+        try {
+            List<String> lines = Files.readAllLines(path);
+            lines.forEach(output::append);
+            return output.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output.toString();
     }
 
     @Override
     public boolean write(String nome, String conteudo) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Path path = Paths.get(nome);
+        if(Files.notExists(path) || !Files.isRegularFile(path)) return false;
+        File file = new File(String.valueOf(path));
+
+        if(!file.canWrite()) return false;
+
+        try {
+            PrintStream ps = new PrintStream(file);
+            ps.append(conteudo);
+            ps.flush();
+            ps.close();
+            return true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 
     @Override
     public void sair() throws RemoteException {
         unexportObject(this, true);
     }
-
-    // PARA TESTES
-    public static void main(String[] args) {
-
-        try {
-            Navegador n = new Navegador();
-//            System.out.println(n.cd("/home/bruno/"));
-//            System.out.println(Arrays.toString(n.ls()));
-            System.exit(0);
-        } catch (RemoteException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-
 }
