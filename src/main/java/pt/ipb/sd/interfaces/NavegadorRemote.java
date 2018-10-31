45aa63756bd77588e28fb8ae5668c071247d7189
Bruno Mendes <brunoms.50@gmail.com>
2018-10-31 14:02:01 +0000

    NavegadorRemote

--- src/main/java/pt/ipb/sd/interfaces/NavegadorRemote.java
+++ src/main/java/pt/ipb/sd/interfaces/NavegadorRemote.java
@@ -1,5 +1,29 @@
 package pt.ipb.sd.interfaces;
 
-public interface NavegadorRemote {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface NavegadorRemote extends Remote {
+
+    public String cd(String diretoria) throws RemoteException;
+
+    public List<String> ls() throws RemoteException;
+
+    public boolean mkdir(String nome) throws RemoteException;
+
+    public boolean touch(String nome) throws RemoteException;
+
+    public boolean rm(String nome) throws RemoteException;
+
+    public boolean rmdir(String nome) throws RemoteException;
+
+    public String file(String nome) throws RemoteException;
+
+    public String cat(String nome) throws RemoteException;
+
+    public boolean write(String nome, String conteudo) throws RemoteException;
+
+    public void sair() throws RemoteException;
 
 }
