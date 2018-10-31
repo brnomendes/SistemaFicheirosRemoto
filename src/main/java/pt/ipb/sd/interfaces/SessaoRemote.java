9818a96fa21f40f168f0267948c62ceb000270d5
Bruno Mendes <brunoms.50@gmail.com>
2018-10-31 14:18:36 +0000

    SessaoRemote

--- src/main/java/pt/ipb/sd/interfaces/SessaoRemote.java
+++ src/main/java/pt/ipb/sd/interfaces/SessaoRemote.java
@@ -3,7 +3,7 @@ package pt.ipb.sd.interfaces;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface Sessao extends Remote {
+public interface SessaoRemote extends Remote {
 
     public NavegadorRemote login() throws RemoteException;
 }
