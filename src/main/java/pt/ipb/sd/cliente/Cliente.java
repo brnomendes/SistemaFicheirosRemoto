9cf38cd9fd8ee6c1171c44e0193b1fa404698284
Bruno Mendes <brunoms.50@gmail.com>
2018-10-31 15:16:28 +0000

    Cliente

--- src/main/java/pt/ipb/sd/cliente/Cliente.java
+++ src/main/java/pt/ipb/sd/cliente/Cliente.java
@@ -1,5 +1,41 @@
 package pt.ipb.sd.cliente;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import pt.ipb.sd.interfaces.NavegadorRemote;
+import pt.ipb.sd.interfaces.SessaoRemote;
+
 public class Cliente {
+
+    public static String NAME = "SESSAO";
+    public static int PORT = 1099;
+
+    public static void main(String[] args) {
+
+        if (args.length < 1) {
+            System.err.println("O IP do servidor deve ser passado por parametro");
+            System.exit(-1);
+        }
+        System.getProperties().setProperty("java.security.policy", "security.policy");
+        System.getProperties().setProperty("java.rmi.server.useCodebaseOnly", "false");
+
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+
+        try {
+            LocateRegistry.getRegistry(PORT);
+            SessaoRemote sessao = (SessaoRemote) Naming.lookup(NAME);
+            NavegadorRemote nr = sessao.login();
+
+            new ClienteGUI(nr).setVisible(true);
+
+        } catch (RemoteException | MalformedURLException | NotBoundException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- src/main/java/pt/ipb/sd/cliente/ClienteGUI.java
+++ src/main/java/pt/ipb/sd/cliente/ClienteGUI.java
@@ -1,38 +1,99 @@
 package pt.ipb.sd.cliente;
 
+import pt.ipb.sd.interfaces.NavegadorRemote;
+
 public class ClienteGUI extends javax.swing.JFrame {
 
-    public ClienteGUI() {
-        initComponents();
-    }
+    private final NavegadorRemote nr;
 
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(() -> {
-            new ClienteGUI().setVisible(true);
-        });
+    public ClienteGUI(NavegadorRemote nr) {
+        initComponents();
+        this.nr = nr;
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        arvorePanel = new javax.swing.JScrollPane();
+        arvore = new javax.swing.JTree();
+        txtInfo = new javax.swing.JTextField();
+        ficheiroPanel = new javax.swing.JScrollPane();
+        txtFicheiro = new javax.swing.JTextArea();
+        btnVoltar = new javax.swing.JButton();
+        btnNovoFicheiro = new javax.swing.JButton();
+        btnNovaPasta = new javax.swing.JButton();
+        btnSalvar = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        arvorePanel.setViewportView(arvore);
+
+        txtFicheiro.setColumns(20);
+        txtFicheiro.setRows(5);
+        ficheiroPanel.setViewportView(txtFicheiro);
+
+        btnVoltar.setText("Voltar");
+
+        btnNovoFicheiro.setText("Novo Ficheiro");
+
+        btnNovaPasta.setText("Nova Pasta");
+
+        btnSalvar.setText("Salvar");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(arvorePanel, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnVoltar))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnNovoFicheiro)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnNovaPasta)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 175, Short.MAX_VALUE)
+                        .addComponent(btnSalvar))
+                    .addComponent(txtInfo)
+                    .addComponent(ficheiroPanel))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(txtInfo, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(ficheiroPanel))
+                    .addComponent(arvorePanel, javax.swing.GroupLayout.PREFERRED_SIZE, 402, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 8, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnVoltar)
+                    .addComponent(btnNovoFicheiro)
+                    .addComponent(btnNovaPasta)
+                    .addComponent(btnSalvar))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTree arvore;
+    private javax.swing.JScrollPane arvorePanel;
+    private javax.swing.JButton btnNovaPasta;
+    private javax.swing.JButton btnNovoFicheiro;
+    private javax.swing.JButton btnSalvar;
+    private javax.swing.JButton btnVoltar;
+    private javax.swing.JScrollPane ficheiroPanel;
+    private javax.swing.JTextArea txtFicheiro;
+    private javax.swing.JTextField txtInfo;
     // End of variables declaration//GEN-END:variables
 
 }
